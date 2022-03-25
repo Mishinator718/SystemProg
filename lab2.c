b0fbad7dc6c42eddbdeb5181927dce16e4d26f5a
Michael <pesnyak00@gmail.com>
2022-03-25 13:00:25 +0700

    C

---
+++ lab2.c
@@ -0,0 +1,10 @@
+int main(void) { /// rbp 80
+int arr[10]; 
+int sevenByteMask = 0b00000001;
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i] & sevenByteMask) {
+            arr[i] = arr[i];
+                                    }
+                                }
+
+}
